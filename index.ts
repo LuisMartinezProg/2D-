54d2b9d1f3d862d895e4fc241684e03345e43e0b
LuisMartinezProg <santiagomartinez29nov@gmail.com>
2026-07-16 18:48:43 -0500

    Add files via upload

---
+++ index.ts
@@ -0,0 +1,5 @@
+export { Vector2 } from "./vector2.js";
+export { Matrix2D } from "./matrix2d.js";
+export { type Rect, rectsIntersect } from "./rect.js";
+export { Easing } from "./easing.js";
+export { lerp, clamp, degToRad, radToDeg } from "./utils.js";
