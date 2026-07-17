4c2949b2f042541f1799c69c5f4379af3df1920b
LuisMartinezProg <santiagomartinez29nov@gmail.com>
2026-07-17 07:13:05 -0500

    Refactor exports in index.ts for new components

--- src/index.ts
+++ src/index.ts
@@ -1,2 +1,9 @@
-export { EventBus } from './EventBus';
-export type { EventPayload, EventCallback } from './types';
+export { Renderer } from './Renderer';
+export { Layer, type LayerDrawEntry, type CameraView, type CanvasFactory } from './Layer';
+export { WorldTransformResolver } from './WorldTransformResolver';
+export { CameraController, clampToBounds } from './CameraController';
+export { Sprite } from './components/Sprite';
+export { Camera } from './components/Camera';
+export type { AssetManager } from './AssetManager';
+export { RendererEvents, type RendererResizedPayload } from './events';
+export type { RendererConfig, ResolvedTransform } from './types';
