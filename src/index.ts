9240ed9906379e9da1516ab430de0e3ee3ad9566
LuisMartinezProg <santiagomartinez29nov@gmail.com>
2026-07-16 19:45:01 -0500

    Update exports in index.ts for EventBus and types

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,2 @@
-export { GameLoop, type EventBus } from './GameLoop';
-export { TimeAccumulator } from './TimeAccumulator';
-export { GameLoopEvents } from './events';
-export type { GameLoopConfig, FixedUpdateCallback, RenderCallback } from './types';
-export { DEFAULT_CONFIG } from './types';
+export { EventBus } from './EventBus';
+export type { EventPayload, EventCallback } from './types';
