f63dc60a292778ad2eb3b543824779d7902c3fde
LuisMartinezProg <santiagomartinez29nov@gmail.com>
2026-07-16 19:12:53 -0500

    Refactor exports in index.ts for better structure

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,10 @@
-export { Vector2 } from "./vector2.js";
-export { Matrix2D } from "./matrix2d.js";
-export { type Rect, rectsIntersect } from "./rect.js";
-export { Easing } from "./easing.js";
-export { lerp, clamp, degToRad, radToDeg } from "./utils.js";
+export type { EntityId, ComponentClass, QueryResult, System } from './types';
+export { World } from './World';
+export { SparseSet } from './SparseSet';
+export { Transform } from './components/Transform';
+export {
+  EcsEvents, noopEventEmitter,
+  type EcsEventEmitter,
+  type EntityCreatedPayload, type EntityDestroyedPayload,
+  type ComponentAddedPayload, type ComponentRemovedPayload,
+} from './events';
