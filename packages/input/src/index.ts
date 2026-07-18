23b5f1576b8a9498bad8bd636fd660c9ccfa0124
LuisMartinezProg <santiagomartinez29nov@gmail.com>
2026-07-18 12:40:07 -0500

    Export input-related modules and types from index.ts

---
+++ packages/input/src/index.ts
@@ -0,0 +1,4 @@
+export { InputManager } from './InputManager';
+export { VirtualJoystick } from './VirtualJoystick';
+export { InputEvents } from './InputEvents';
+export type { TouchPoint } from './types';
