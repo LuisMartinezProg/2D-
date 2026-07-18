9b9eb5846520cfee89e494d47cb7f3587d4c2db1
LuisMartinezProg <santiagomartinez29nov@gmail.com>
2026-07-18 14:12:31 -0500

    Add exports for math utilities and types

---
+++ packages/math/src/index.ts
@@ -0,0 +1,6 @@
+export { Vector2 } from './vector2';
+export { Matrix2D } from './matrix2d';
+export { Easing } from './easing';
+export type { Rect } from './rect';
+export { rectsIntersect } from './rect';
+export { lerp, clamp, degToRad, radToDeg } from './utils';
