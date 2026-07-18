ed4af6f3652cbf336fbc11e4fcddd5d4a0fd59ad
LuisMartinezProg <santiagomartinez29nov@gmail.com>
2026-07-18 11:58:37 -0500

    Export modules and types from index.ts

---
+++ packages/ecs/src/index.ts
@@ -0,0 +1,7 @@
+export { World } from './World';
+export { SparseSet } from './SparseSet';
+export { LazyQueryResult, EMPTY_QUERY_RESULT } from './QueryResult';
+export { Transform } from './components/Transform';
+export type { EntityId, ComponentClass, QueryResult, System } from './types';
+export { EcsEvents, noopEventEmitter } from './events';
+export type { EcsEventEmitter } from './events';
