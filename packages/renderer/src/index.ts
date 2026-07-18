6918c81e3f29c23b2b1372b5e645d58b355ed4c1
LuisMartinezProg <santiagomartinez29nov@gmail.com>
2026-07-18 17:51:06 -0500

    Add index.ts to export various modules and types

---
+++ packages/renderer/src/index.ts
@@ -0,0 +1,8 @@
+export { Renderer } from './Renderer';
+export { Layer, type LayerDrawEntry, type CameraView, type CanvasFactory } from './Layer';
+export { WorldTransformResolver } from './WorldTransformResolver';
+export { CameraController, clampToBounds } from './CameraController';
+export { Sprite } from './components/Sprite';
+export { Camera } from './components/Camera';
+export { RendererEvents, type RendererResizedPayload } from './events';
+export type { RendererConfig, ResolvedTransform } from './types';
