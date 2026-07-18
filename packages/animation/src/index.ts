36259f0cb53cd2a074df13a00337bbd1840ec0f7
LuisMartinezProg <santiagomartinez29nov@gmail.com>
2026-07-18 12:40:32 -0500

    Export animation-related modules from index.ts

---
+++ packages/animation/src/index.ts
@@ -0,0 +1,4 @@
+export { Animator } from './Animator';
+export { AnimationSystem } from './AnimationSystem';
+export { playAnimation } from './controls';
+export { AnimationEvents } from './AnimationEvents';
