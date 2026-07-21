07a21af3516ef5a81473fb7bbf93cac001b0deb0
LuisMartinezProg <santiagomartinez29nov@gmail.com>
2026-07-21 16:06:18 -0500

    Refactor Camera class to use optional bounds

--- packages/renderer/src/components/Camera.ts
+++ packages/renderer/src/components/Camera.ts
@@ -1,9 +1,7 @@
 import type { EntityId } from '@mochigo/ecs';
 import type { Rect } from '@mochigo/math';
-import { Rect } from '@mochigo/math';
 
 export class Camera {
-  bounds: Rect;
   static readonly componentName = 'Camera';
 
   constructor(
