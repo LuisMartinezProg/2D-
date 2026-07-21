33b96f85e106a6d79a08c7e0b61f4795b5611911
LuisMartinezProg <santiagomartinez29nov@gmail.com>
2026-07-21 16:05:28 -0500

    Add parent property to Transform class

--- packages/ecs/src/components/Transform.ts
+++ packages/ecs/src/components/Transform.ts
@@ -1,17 +1,23 @@
 import { Vector2 } from '@mochigo/math';
+import type { EntityId } from './EntityId';
 
 export class Transform {
+  static readonly componentName = 'Transform';
+
   position: Vector2;
   rotation: number;
   scale: Vector2;
+  parent: EntityId | null;
 
   constructor(
     position: Vector2 = Vector2.zero(),
     rotation: number = 0,
-    scale: Vector2 = Vector2.one()
+    scale: Vector2 = Vector2.one(),
+    parent: EntityId | null = null
   ) {
     this.position = position;
     this.rotation = rotation;
     this.scale = scale;
+    this.parent = parent;
   }
 }
