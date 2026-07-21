b24d0d469b9bf4e6f666bca5c2b31ecc73078875
LuisMartinezProg <santiagomartinez29nov@gmail.com>
2026-07-21 15:47:52 -0500

    Refactor exports in index.ts for consistency

--- packages/math/src/index.ts
+++ packages/math/src/index.ts
@@ -1,8 +1,6 @@
-
 export { Matrix2D } from './matrix2d';
 export { Easing } from './easing';
+export { lerp, clamp, degToRad, radToDeg } from './utils';
+export { Vector2 } from './vector2';
 export type { Rect } from './rect';
 export { rectsIntersect } from './rect';
-export { lerp, clamp, degToRad, radToDeg } from './utils';
-export { Vector2 } from './Vector2';
-export { Rect, rectsIntersect } from './Rect';
